5dbd2c5b169a620faf47a1a8b65a19caaaa33211
AABB2741 <mariosantos88477364484@gmail.com>
2025-07-07 22:53:06 -0300

    feat: init backend project

---
+++ apps/server/src/server.ts
@@ -0,0 +1,24 @@
+import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { env } from "./env.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: env.WEB_URL,
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", () => "Ok");
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("🚀 HTTP server running!");
+});
